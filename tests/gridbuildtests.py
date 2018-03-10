8e29fbe4ef084ff66b34e6a8cd976ba1f933fd45
mutetext <eeisenberg0@gmail.com>
2018-03-09 23:03:55 -0500

    Expanded test cases for gridbuild

--- tests/gridbuildtests.py
+++ tests/gridbuildtests.py
@@ -111,4 +111,43 @@ def testFillGridWithTilesOfEveryTypeSuccessValidation():
         assert(grid[6] == '1') == True, '%s has an unknown char where there should be a \'1\'!' % str(grid)
         assert(grid[4] == '') == True, '%s has an unknown char where there should be a \'\'!' % str(grid)
 
-# placeTiles, validateGridHelper, findPlayerPosition to be added
+def testPlaceTilesWithCorrectNumberOfTilesSuccessValidation():
+    grid = numpy.zeros((4, 4), str)
+    grid = gridbuild.placeTiles(grid, 16, 'P')
+    grid = numpy.reshape(grid, (1, 16))[0]
+    grid = numpy.sort(grid)
+    assert(grid[15] == 'P') == True, '%s does not have a \'P\' at its end' % str(grid)
+    assert(grid[0] == 'P') == True, '%s does not have a \'P\' at its beginning' % str(grid)
+
+def testBoardIsPlayableSuccessValidation():
+    ## TEST GRID
+    ## 1	S	''
+    ## P	U	''
+    ## ''	T	1
+    grid = numpy.zeros((3, 3), str)
+    grid[0][1] = 'S'
+    grid[1][0] = 'P'
+    grid[2][1] = 'T'
+    grid[2][2] = '1'
+    grid[0][0] = '1'
+    x = 1
+    y = 1
+    assert(gridbuild.boardIsPlayable(grid, x, y, [], [])) == True, '%s is failing as a playable board' % str(grid)
+    ##TODO: ADD MORE COMPLICATED TEST
+
+def testBoardIsPlayableFailValidation():
+    ## TEST GRID
+    ## T	S	''
+    ## P	U	''
+    ## ''	1	1
+    grid = numpy.zeros((3, 3), str)
+    grid[0][1] = 'S'
+    grid[1][0] = 'P'
+    grid[2][1] = '1'
+    grid[2][2] = '1'
+    grid[0][0] = 'T'
+    x = 1
+    y = 1
+    assert(gridbuild.boardIsPlayable(grid, x, y, [], [])) == False, '%s is failing as a non-playable board' % str(grid)
+    ##TODO: ADD MORE COMPLICATED TEST
+# boardWithoutAdjacentEndTiles, findPlayerPosition to be added
